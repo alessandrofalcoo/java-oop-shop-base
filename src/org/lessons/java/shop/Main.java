a9b4b0c15b2174e219ddd948903ebdd2bf32b10b
alessandrofalcoo <alefalco.1003@gmail.com>
2025-06-12 15:59:42 +0200

    Enhance Product class with getters and setters; update Main class to utilize new Product methods for input handling

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Product product = new Product();
         Scanner inputName = new Scanner(System.in);
+        Scanner inputCode = new Scanner(System.in);
+        Scanner inputPrice = new Scanner(System.in);
+        System.out.println("Inserisci il codice del prodotto");
+        int codice = inputCode.nextInt();
         System.out.println("Inserisci il nome del prodotto: ");
-        product.name = inputName.nextLine();
+        String nome = inputName.nextLine();
+        System.out.println("Inserisci il prezzo del prodotto");
+        float price = inputPrice.nextFloat();
+        Product product = new Product(codice);
+        product.setName(nome);
+        product.setPrice(price);
+        inputCode.close();
         inputName.close();
-        product.setCode();
-        product.setPrice();
-        product.setFullPrice();
-        product.setFullName();
-        System.out.println(product.code);
-        System.out.println(product.price);
-        System.out.println(product.setFullPrice());
-        System.out.println(product.setFullName());
+        inputPrice.close();
+        /*
+         * System.out.println(product.getCode());
+         * System.out.println(product.getPrice());
+         */ System.out.println(product.FullPrice());
+        System.out.println(product.FullName());
     }
 }
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,25 +1,65 @@
 package org.lessons.java.shop;
 
 public class Product {
-    int code;
-    String name;
-    String description;
-    float price;
-    int iva = 22;
+    private int code;
+    private String name;
+    private String description;
+    private float price;
+    private int iva = 22;
 
-    public void setCode() {
-        this.code = (int) (Math.random() * 1000) + 1;
+    Product(int code) {
+        this.code = code;
     }
 
-    public void setPrice() {
-        this.price = (float) Math.round((Math.random() * 10000) + 1 * 100) / 100;
+    Product(int code, String name, String description, float price, int iva) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
     }
 
-    public float setFullPrice() {
+    public int getCode() {
+        return this.code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getIva() {
+        return this.iva;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
+    public float FullPrice() {
         return Math.round((price + (price * iva / 100)) * 100) / 100f;
     }
 
-    public String setFullName() {
+    public String FullName() {
         return this.code + "-" + this.name;
     }
 }
